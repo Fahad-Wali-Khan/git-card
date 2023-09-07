37e33b5fb05470ba7dee1ff658461d515fe50cc0
Fahad-Wali-Khan <94318061+Fahad-Wali-Khan@users.noreply.github.com>
2023-09-07 14:34:34 +0530

    yeh bhi bs

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ const App = () => {
             className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
             onClick={fetchUserData}
           >
-            <button>jagsdh</button>
+            <button>ja</button>
             Fetch
           </button>
         </div>
